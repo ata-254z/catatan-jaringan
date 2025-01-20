2b8327be40c333bf3cc046d41c5ee6419669f1d5
Ata <ririn110283@gmail.com>
2025-01-20 17:08:34 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,1 @@
+window.location.href = "https://www.canva.com/design/DAGcuAHhlwk/KflMO688XOxArhjutXxUHg/edit?utm_content=DAGcuAHhlwk&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton";
