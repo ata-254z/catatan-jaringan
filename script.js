0f0c3e4951e24e46185b9df856dc2a5912e32cc7
Ata <ririn110283@gmail.com>
2025-01-20 17:11:11 +0700

    Update and rename script.js to index.html

--- script.js
+++
@@ -1,1 +0,0 @@
-window.location.href = "https://www.canva.com/design/DAGcuAHhlwk/KflMO688XOxArhjutXxUHg/edit?utm_content=DAGcuAHhlwk&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton";
